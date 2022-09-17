27f9848a4257327be393fe76a1da442bef61f344
rynzoken <rynzoken125@gmail.com>
2022-09-17 09:49:39 +0800

    first commit

---
+++ resume_prototype-main/lib/const.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
+
+// Text colors
+const cTextHeaderColor = Color(0xff2A2A2A);
+const cTextFeatureColor = Color(0xff707070);
+const cTextLabelColor = Colors.white;
+
+// Detail for profile item
+const cProfileTopic = 'About Me';
+const cProfileFeature =
+    "Name: Laurynz M Idos\nAge: 27 years old\nAddress:#18 Buenlag,Calasiao,Pang.\nContact No.:0926-003-7456\nDate of Birth:January, 04, 1995\n" +
+        "Citizenship: Filipino\n" +
+        "Civil Status: Single\nSex: Male";
+const cProfileLabel = 'Profile';
+const cProfilePicture = 'assets/images/avatar.png';
+const cProfileIconImage = 'assets/images/profile.png';
+const cProfileColor = Color(0xFF004D40);
+const cProfileBegColor = Color(0xFF00796B);
+const cProfileEndColor = Color(0xFF4DB6AC);
+
+// Detail for education item
+const cEducationTopic = 'Educational Background';
+const cEducationFeature =
+    'Bachelor of Information Technology\nPhilippine College of Science and Technology-March 2019-2023\n' +
+        'Certificates:\n-NCII Computer System Servicing\n-Game Dev\n-Road to success';
+const cEducationLabel = 'Education';
+const cEducationPicture = 'assets/images/book.png';
+const cEducationIconImage = 'assets/images/education.png';
+const cEducationColor = Color(0xFF3E2723);
+const cEduBegColor = Color(0xFF795548);
+const cEduEndColor = Color(0xFFBCAAA4);
+
+// Detail for experience item
+const cExperienceTopic = 'Work Experience';
+const cExperienceFeature = 'Trainer(CSS-JDVP) - Litec\n' +
+    'Dec. 2021 to march 2022 \n' +
+    '- Technical support Technicians\n' +
+    '- Network support Technicians';
+const cExperienceLabel = 'Experience';
+const cExperiencePicture = 'assets/images/laptop.png';
+const cExperienceIconImage = 'assets/images/experience.png';
+const cExperienceColor = Color(0xFF263238);
+const cExpBegColor = Color(0xFF546E7A);
+const cExpEndColor = Color(0xFF90A4AE);
+
+final cCaptionTextSize = 20.0.sp;
+const cStartingIndex = 0;
